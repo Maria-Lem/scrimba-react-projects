c58499d00797ca52ad419d9f0f43457d8cdfb4b1
Maria Alemaskina <maria131621@yandex.ru>
2022-09-11 13:04:57 +0300

    feat: add HTML layout of the first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './images/react-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img className="logo" src={logo} alt="logo" width="46px" />
       </header>
+      <main className="main">
+        <h1 className="title">Fun facts about React</h1>
+        <ul>
+          <li className="item">Was first released in 2013</li>
+          <li className="item">Was originally created by Jordan Walke</li>
+          <li className="item">Has well over 100K stars on GitHub</li>
+          <li className="item">Is maintained by Facebook</li>
+          <li className="item">Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
     </div>
   );
 }
