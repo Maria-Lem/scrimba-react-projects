16c4c708d426b14561705325f100408c0fecad55
Maria Alemaskina <maria131621@yandex.ru>
2022-09-12 15:28:56 +0300

    feat: add header styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/navbar';
 import Main from './components/main';
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import logo from "../images/react-logo.png";
+import navbarStyles from "../styles/navbarStyles.module.css";
 
 function Navbar() {
   return (
-    <h1>Hello</h1>
+    <header className={navbarStyles.header}>
+      <div className={navbarStyles.container}>
+        <div className={navbarStyles.logo}>
+          <img className={navbarStyles.logoImg} src={logo} alt="logo" />
+          <h2 className={navbarStyles.logoText}>ReactFacts</h2>
+        </div>
+        <h3 className={navbarStyles.title}>React Course - Project 1</h3>
+      </div>
+    </header>
   )
 }
 
