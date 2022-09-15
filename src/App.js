1e5d98bb254c67c0255d192e872fd69018d1cd74
Maria Alemaskina <maria131621@yandex.ru>
2022-09-15 14:23:54 +0300

    feat: add HTML layout and styles

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ function App() {
     <div className="App">
       <Navbar />
       <Main />
-      {/* <header className="header">
-        <img className="logo" src={logo} alt="logo" width="46px" />
-      </header>
-      <main className="main">
-        <h1 className="title">Fun facts about React</h1>
-        <ul>
-          <li className="item">Was first released in 2013</li>
-          <li className="item">Was originally created by Jordan Walke</li>
-          <li className="item">Has well over 100K stars on GitHub</li>
-          <li className="item">Is maintained by Facebook</li>
-          <li className="item">Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main> */}
     </div>
   );
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,20 @@
 import React from "react";
+import mainStyles from "../styles/mainStyles.module.css";
 
 function Main() {
   return (
-    <p>Lorem</p>
+      <main className={mainStyles.main}>
+        <div className={mainStyles.container}>
+          <h1 className={mainStyles.title}>Fun facts about React</h1>
+          <ul>
+            <li className={mainStyles.item}>Was first released in 2013</li>
+            <li className={mainStyles.item}>Was originally created by Jordan Walke</li>
+            <li className={mainStyles.item}>Has well over 100K stars on GitHub</li>
+            <li className={mainStyles.item}>Is maintained by Facebook</li>
+            <li className={mainStyles.item}>Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
+        </div>
+      </main>
   )
 }
 
